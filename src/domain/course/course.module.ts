be6931c625c66943d6cf41543116dc738b7ec513
gilgeunwoo <kesksjxhx@gmail.com>
2023-12-29 08:18:55 +0900

    feat :: create course api

--- src/domain/course/course.module.ts
+++ src/domain/course/course.module.ts
@@ -1,13 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from './course.entity';
+import { CreateCourseUseCase } from './usecase/create-course.usecase';
+import { CourseController } from './presentation/course.controller';
 
 const COURSE_REPOSITORY = TypeOrmModule.forFeature([CourseEntity]);
 
 @Global()
 @Module({
     imports: [COURSE_REPOSITORY],
-    exports: [COURSE_REPOSITORY]
+    exports: [COURSE_REPOSITORY],
+    providers: [CreateCourseUseCase],
+    controllers: [CourseController]
 })
 export class CourseModule {
 
