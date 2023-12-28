857db0e84da70669b4817ed076e1bfb143f2d276
woonseok105 <yws3786@gmail.com>
2023-12-28 22:28:48 +0900

    feat :: 강의 엔티티 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './domain/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { TypeormConfigModule } from './global/config/typeorm.config';
 import { UserModule } from './domain/user/user.module';
 import { CourseModule } from './domain/course/course.module';
+import { ConfigModule } from '@nestjs/config';
+import { LectureModule } from './domain/lecture/lecture.module';
 
 @Module({
     imports: [
         TypeormConfigModule,
         AuthModule,
         UserModule,
+        LectureModule,
         CourseModule,
         ConfigModule.forRoot({ isGlobal: true })
     ]
